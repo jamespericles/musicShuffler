8e0cd58bf997ed69ed448dea15972f4ad8bfb1a4
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-05 16:02:38 -0400

    linked storyboard elements to code

---
+++ musicShuffler/GenreButtonScreen.swift
@@ -0,0 +1,23 @@
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    @IBAction func genraButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+}
