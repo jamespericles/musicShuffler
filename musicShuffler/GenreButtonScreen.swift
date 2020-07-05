6098f6cb6c862ed4c80bd1c9fd64b26c8990886d
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-05 16:10:13 -0400

    added controls for buttons and music player

--- musicShuffler/GenreButtonScreen.swift
+++ musicShuffler/GenreButtonScreen.swift
@@ -4,7 +4,7 @@ import MediaPlayer
 
 class GenreButtonScreen: UIViewController {
     
-    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer()
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,11 +13,28 @@ class GenreButtonScreen: UIViewController {
     }
     
     @IBAction func genraButtonTapped(_ sender: UIButton) {
+        playGenre(genre: sender.currentTitle!)
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        playGenre(genre: sender.currentTitle!)
     }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        playGenre(genre: sender.currentTitle!)
     }
+    
+    func playGenre(genre: String) {
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+    
 }
