d44956b24a21825ffacfea3088bf07a624ab8f0f
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-04 20:00:45 -0400

    Initial Commit

---
+++ musicShuffler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  musicShuffler
+//
+//  Created by James Pericles II on 7/4/20.
+//  Copyright © 2020 James Pericles II. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
